d8ae690cde5984181e1fa3627d2c3494bc93ddee
Ray <sugar.ray.m.tenorio@accenture.com>
2017-08-14 14:10:29 +0800

    google direcoory service basics

--- app/components/directory.py
+++ app/components/directory.py
@@ -2,15 +2,21 @@ from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient import discovery
 from core.config import Config
 
-_google_directory = discovery.build('admin', 'directory_v1', credentials=(
-    ServiceAccountCredentials.from_json_keyfile_name(Config.get('CREDENTIALS_FILE_PATH')) \
-        .create_scoped(Config.get('SCOPES')).create_delegated(Config.get('DELEGATED_USER'))
-))
 
+class DirectoryService(object):
+    service = None
 
-def get_user_list(group_email):
-    return _google_directory.members().list(groupKey=group_email).execute()
+    def __init__(self):
+        self.create_service()
 
+    def create_service(self):
+        self.service = discovery.build('admin', 'directory_v1', credentials=(
+            ServiceAccountCredentials.from_json_keyfile_name(Config.get('CREDENTIALS_FILE_PATH')) \
+                .create_scoped(Config.get('SCOPES')).create_delegated(Config.get('DELEGATED_USER'))
+        ))
 
-def get_user_details(user_email):
-    return _google_directory.users().get(userKey=user_email, projection="full").execute()
+    def get_user_list(self, group_email):
+        return self.service.members().list(groupKey=group_email).execute()
+
+    def get_user_details(self, user_email):
+        return self.service.users().get(userKey=user_email, projection="full").execute()
